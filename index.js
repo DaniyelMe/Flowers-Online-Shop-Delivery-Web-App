3f84263e4296e11964e703c66fa26bbf2bb57c50
Daniyel <DaniyelMenda@Gmail.com>
2017-11-29 13:37:18 +0200

    Login
    - Login page, not implemented yet
    - Folder arrangement

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
 const app = express();
 
 app.use((req, res, next) => {
@@ -27,6 +29,18 @@ app.get('/branches', (req, res) => {
 
 
 
+	fs.readFile('./assets/templates/index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});  // <-- HERE!
+        response.write(html);  // <-- HERE!
+        response.end();
+    }).listen(3000, '127.0.0.1');
+
 app.listen(3000, () => {
 	console.log('server running');
+	});
+
 });
